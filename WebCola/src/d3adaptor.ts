b1fc8bf7d637c6a9f8e92847a62724852251fd70
Maarten Rijke <maartenrijke@gmail.com>
2018-06-13 11:11:29 +0200

    style: Run prettier over all code

--- WebCola/src/d3adaptor.ts
+++ WebCola/src/d3adaptor.ts
@@ -1,24 +1,28 @@
-import * as d3v3 from './d3v3adaptor'
-import * as d3v4 from './d3v4adaptor'
-import { Layout, EventType, Event } from './layout';
+import * as d3v3 from "./d3v3adaptor";
+import * as d3v4 from "./d3v4adaptor";
+import { Layout, EventType, Event } from "./layout";
 
-export interface D3v3Context { version:string };
-
-export interface ID3StyleLayoutAdaptor {
-    trigger(e: Event): void;
-    kick(): void;
-    drag: () => any;
-
-    on(eventType: EventType | string, listener: () => void): ID3StyleLayoutAdaptor;
+export interface D3v3Context {
+  version: string;
 }
 
+export interface ID3StyleLayoutAdaptor {
+  trigger(e: Event): void;
+  kick(): void;
+  drag: () => any;
+
+  on(
+    eventType: EventType | string,
+    listener: () => void
+  ): ID3StyleLayoutAdaptor;
+}
 
 /**
  * provides an interface for use with d3:
  * Correct way to create way to construct the d3 cola object is to pass the d3 object into the adaptor function, like so:
- * 
+ *
  *   `var d3cola = cola.d3adaptor(d3);`
- * 
+ *
  * Internally, it will figure out if d3 is version 3 or 4 from the version tag and set up the right event forwarding. Defaults to version 3 if the d3 object is not passed.
  * - uses the d3 event system to dispatch layout events such as:
  *   o "start" (start layout process)
@@ -30,14 +34,20 @@ export interface ID3StyleLayoutAdaptor {
  * returns an instance of the cola.Layout itself with which the user
  * can interact directly.
  */
-export function d3adaptor(d3Context?: d3v4.D3Context | D3v3Context): Layout & ID3StyleLayoutAdaptor {
-    if (!d3Context || isD3V3(d3Context)) {
-        return new d3v3.D3StyleLayoutAdaptor();
-    }
-    return new d3v4.D3StyleLayoutAdaptor(d3Context);
+export function d3adaptor(
+  d3Context?: d3v4.D3Context | D3v3Context
+): Layout & ID3StyleLayoutAdaptor {
+  if (!d3Context || isD3V3(d3Context)) {
+    return new d3v3.D3StyleLayoutAdaptor();
+  }
+  return new d3v4.D3StyleLayoutAdaptor(d3Context);
 }
 
-function isD3V3(d3Context: d3v4.D3Context | D3v3Context): d3Context is D3v3Context {
-    const v3exp = /^3\./;
-    return (<any>d3Context).version && (<any>d3Context).version.match(v3exp) !== null;
+function isD3V3(
+  d3Context: d3v4.D3Context | D3v3Context
+): d3Context is D3v3Context {
+  const v3exp = /^3\./;
+  return (
+    (<any>d3Context).version && (<any>d3Context).version.match(v3exp) !== null
+  );
 }
